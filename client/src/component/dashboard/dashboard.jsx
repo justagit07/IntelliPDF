10c65fb6f55d31dff0cd434716c23c24ccc803c8
justagit07 <droptelegram@outlook.com>
2024-04-02 00:00:06 +0530

    drag and drop of the pdf is working

--- client/src/component/dashboard/dashboard.jsx
+++ client/src/component/dashboard/dashboard.jsx
@@ -1,6 +1,21 @@
 import React, { useState } from 'react'
-
+import {useDropzone} from 'react-dropzone';
+import axios from 'axios';
 export default function Dashboard() {
+
+        const onDrop = (acceptedFiles) => {
+
+          console.log(acceptedFiles);
+
+        
+
+
+
+
+
+
+        }
+        const {getRootProps, getInputProps} = useDropzone({onDrop, accept: '.pdf'});
 
     const [showDiv, setShowDiv]= useState(false)
   return (
@@ -18,31 +33,49 @@ export default function Dashboard() {
 
      </nav>
 
-     <div className='  w-full  items-center content-center '>
+     <div className='  w-full  items-center  content-center '>
       <div className='m-12 border-b-2 p-4 flex justify-around'>
          <p className='text-4xl font-bold'>My Files</p>
          <div className='p-2 rounded-md bg-blue-600 w-[100px]'>
             <button className=' text-center align-middle content-center text-white ' onClick={() => setShowDiv(true)}> 
-         Upload pdf
+             Add PDF
             </button>
             
-            </div>
-            
+            </div>      
+
       </div>
+             <div {...getRootProps()} className="dropzone">
+      <div className='w-[50vw] h-[30vh] p-3 relative left-[23vw]  rounded-lg bg-white '>
+
+        <div className='w-[48vw] h-[26vh]   content-center   mt-1 rounded-lg border-2 border-dashed '>
+             <div className='flex justify-center my-3' >
+      <input {...getInputProps()} />
+      
+             <img src="../../src/assets/pdfs.svg"    alt="hnji" />
+             </div>
+
+             <p className='text-center'>Drag and drop single pdf here</p>
+            <div className='items-center content-center  my-3 flex justify-center'>
+
+       <button className='flex justify-center items-center content-center w-36 h-10 rounded-lg bg-[#2563EB] '>
+           <i className="fi fi-rr-file-pdf invert  m-2" ></i>
+         <div className=' font-semibold items-center text-white text-center '> 
+
+         Upload Files
+
+             </div>
+             </button>
+              </div>
+              </div>
+
+        </div>
+        
+         </div>
+
+
      </div>
     </div>
-
-
-
-
-
-
-
-
-
-
 
     </>
   )
 }
-
